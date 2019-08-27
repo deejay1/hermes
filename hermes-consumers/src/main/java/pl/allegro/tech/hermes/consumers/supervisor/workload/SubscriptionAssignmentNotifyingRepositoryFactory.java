fc37951c7df39a6e6697ca9b59c50a116ae2b3d7
Daniel Krawczyk <daniel.krawczyk@allegro.pl>
2019-08-27 13:11:42 +0200

    Consumer workload registry type

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workload/SubscriptionAssignmentNotifyingRepositoryFactory.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workload/SubscriptionAssignmentNotifyingRepositoryFactory.java
@@ -2,13 +2,19 @@ package pl.allegro.tech.hermes.consumers.supervisor.workload;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.glassfish.hk2.api.Factory;
+import org.slf4j.Logger;
 import pl.allegro.tech.hermes.common.config.ConfigFactory;
+import pl.allegro.tech.hermes.common.config.Configs;
 import pl.allegro.tech.hermes.consumers.subscription.cache.SubscriptionsCache;
 import pl.allegro.tech.hermes.infrastructure.zookeeper.ZookeeperPaths;
 
 import javax.inject.Inject;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class SubscriptionAssignmentNotifyingRepositoryFactory implements Factory<SubscriptionAssignmentNotifyingRepository> {
+
+    private static final Logger logger = getLogger(SubscriptionAssignmentNotifyingRepositoryFactory.class);
 
     private final CuratorFramework curator;
     private final ConfigFactory configFactory;
@@ -30,7 +36,24 @@ public class SubscriptionAssignmentNotifyingRepositoryFactory implements Factory
 
     @Override
     public SubscriptionAssignmentNotifyingRepository provide() {
-        return new SubscriptionAssignmentCache(curator, configFactory, zookeeperPaths, subscriptionsCache);
+        ConsumerWorkloadRegistryType type;
+        try {
+            String typeString = configFactory.getStringProperty(Configs.CONSUMER_WORKLOAD_REGISTRY_TYPE);
+            type = ConsumerWorkloadRegistryType.fromString(typeString);
+        } catch (Exception e) {
+            logger.error("Could not configure subscription assignment notifying repository based on specified consumer workload registry type", e);
+            throw e;
+        }
+        logger.info("Consumer workload registry type chosen: {}", type.getConfigValue());
+
+        switch (type) {
+            case HIERARCHICAL:
+                return new SubscriptionAssignmentCache(curator, configFactory, zookeeperPaths, subscriptionsCache);
+            case FLAT_BINARY:
+                throw new UnsupportedOperationException("Not implemented yet");
+            default:
+                throw new UnsupportedOperationException("Max-rate type not supported.");
+        }
     }
 
     @Override
